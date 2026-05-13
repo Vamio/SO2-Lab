af294935a88c847805d52e0e7a21422e40515a98
vamio <vamio@192.168.0.107>
2026-05-13 18:21:10 +0200

    I hope interface has been implemented :(

--- SO2Lab/src/duck2/Duck.java
+++ SO2Lab/src/duck2/Duck.java
@@ -1,10 +1,19 @@
 package duck2;
 
-public class Duck {
+import java.awt.Point;
+
+public class Duck implements LocatedRectangle {
 	private Body body; // composition
 	private Head head; // composition
 	
+	private int x, y, width, height;
+	
 	public Duck(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		
 		body = new Body(x, y, width, height);
 		head = new Head(x - (width / 9), y - (height / 3 * 2), width / 3, height - (height / 4));
 	}
@@ -12,5 +21,24 @@ public class Duck {
 	public void draw() {
 		head.draw();
 		body.draw();
+	}
+
+	@Override
+	public Point address() {
+		// TODO Auto-generated method stub
+		Point location = new Point(x,y);
+		return location;
+	}
+
+	@Override
+	public int width() {
+		// TODO Auto-generated method stub
+		return width;
+	}
+
+	@Override
+	public int height() {
+		// TODO Auto-generated method stub
+		return height;
 	}
 }
